0686bab3ef0d2e96bd2cdf16902db0cf9e84229c
taha_mabn@hotmail.com <taha_mabn@hotmail.com>
2023-01-25 18:56:09 +0300

    chore: add ss and ark, run th projects

--- apps/alaa-restaurant-server/src/app/app.module.ts
+++ apps/alaa-restaurant-server/src/app/app.module.ts
@@ -3,9 +3,43 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { CommonModule, SSConfig } from '@ss/common';
+import { DatabasesCollections, DataModule, DbConnectionOptions } from '@ss/data';
+import { Schema } from 'mongoose'
+import { StorageModule } from "@ss/storage"
+import { AuthModule } from '@ss/auth';
+import { RulesModule } from "@ss/rules"
+import { ScheduleModule } from '@nestjs/schedule';
+import { ApiModule } from '@ss/api';
+
+
+
+const conf = new SSConfig('bus');
+// conf.broker = { transport: Transport.REDIS, options: { host: `${process.env.REDIS_HOST ?? 'localhost'}`, port: tryParseInt(process.env.REDIS_PORT, 6379) } };
+const dbConnectionOptions = new DbConnectionOptions()
+dbConnectionOptions.prefix = 'ss_'
+const collections: DatabasesCollections = {
+
+    'DB_DEFAULT': {
+        collections: {
+            'settings': { schema: new Schema({ _id: String }, { strict: false }) },
+        },
+        dbConnectionOptions
+
+    }
+}
+
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        CommonModule.register(conf,),
+        DataModule.register(collections),
+        AuthModule.register({ secret: "iskambilsecretlolhhh" }),
+        ScheduleModule.forRoot(),
+        StorageModule,
+        ApiModule,
+        RulesModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- apps/alaa-restaurant-server/src/main.ts
+++ apps/alaa-restaurant-server/src/main.ts
@@ -1,22 +1,14 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app/app.module';
+import { bootstrap } from '@ss/common';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
-}
+const services: MicroserviceOptions[] = [
+  
+];
 
-bootstrap();
+bootstrap(AppModule, 3333, {
+    microservices: services,
+    socketAdapter: 'default',
+    applicationName: 'iskambil-game-server'
+})
